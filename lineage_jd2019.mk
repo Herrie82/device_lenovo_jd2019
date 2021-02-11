a9de93c0331b9322a4ce26a10a5e43acf4ab9100
Herrie <Github.com@herrie.org>
2021-02-11 10:36:59 +0100

    Initial changes for Halium 9.0

--- lineage_jd2019.mk
+++ lineage_jd2019.mk
@@ -1,9 +1,10 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/halium.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from jd2019 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
